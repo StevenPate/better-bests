c56686d096934958b56a514643355ab220e54b89
StevenPate <stevenpate@gmail.com>
2023-07-11 21:55:37 -0700

    back out netlify build scheduling

--- netlify/functions/test-function.js
+++
@@ -1,9 +0,0 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/test-function.js
-
-exports.handler = async function(event, context) {
-    console.log("Received event:", event);
-
-    return {
-        statusCode: 200,
-    };
-};
