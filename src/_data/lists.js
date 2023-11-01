32b79f431cd606dbfb2fc650d7060a18a471a3b8
StevenPate <stevenpate@gmail.com>
2023-11-01 11:32:31 -0700

    update dates again

--- src/_data/lists.js
+++ src/_data/lists.js
@@ -159,10 +159,10 @@ module.exports = async function () {
         //     .format(
         //         "YYMMDD"
         //     );
-        const currentDate = "231024"
+        const currentDate = "231101"
 
         // const previousDate = previousDateString(currentDate);
-        const previousDate = "231017";
+        const previousDate = "231024";
         // const postDate = dayjs("20" + currentDate, "YYYYMMDD").format(
         //     "MM-DD-YYYY"
         // );
