febe3ebd15c74ae76cd1721a1bdcd67cadf116aa
StevenPate <stevenpate@gmail.com>
2024-05-15 09:07:16 -0700

    dates

--- src/_data/lists.js
+++ src/_data/lists.js
@@ -161,10 +161,10 @@ module.exports = async function () {
         //     .format(
         //         "YYMMDD"
         //     );
-        const currentDate = "240508"
+        const currentDate = "240515"
 
         // const previousDate = p4reviousDateString(currentDate);
-        const previousDate = "240501";
+        const previousDate = "240508";
         // const postDate = dayjs("20" + currentDate, "YYYYMMDD").format(
         //     "MM-DD-YYYY"
         // );
